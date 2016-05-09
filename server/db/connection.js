35b57c1571830d32b2017a1d2acb76746658dcfe
LisaMabley <lisamabley@gmail.com>
2016-05-09 10:21:19 -0500

    merges still more stashed changes

--- server/db/connection.js
+++ server/db/connection.js
@@ -12,7 +12,7 @@ if (process.env.DATABASE_URL){
 
 function initializeDB() {
   var coopTable = 'CREATE TABLE IF NOT EXISTS coops (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL);';
-  var userTable = 'CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, phone VARCHAR(14), email VARCHAR(30) NOT NULL, coop_id INTEGER REFERENCES coops (id), password VARCHAR(30));';
+  var userTable = 'CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) NOT NULL, phone VARCHAR(14), email VARCHAR(30) UNIQUE NOT NULL, coop_id INTEGER REFERENCES coops (id), password VARCHAR(30));';
   var requestTable = 'CREATE TABLE IF NOT EXISTS requests (id SERIAL PRIMARY KEY, start_time TIMESTAMP NOT NULL, end_time TIMESTAMP NOT NULL, requestor_id INTEGER NOT NULL REFERENCES users (id), caregiver_id INTEGER REFERENCES users (id), comments VARCHAR(120), completed BOOLEAN DEFAULT false);';
 
   pg.connect(connectionString, function(err, client, done) {
