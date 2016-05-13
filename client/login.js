cebd7e2ec09439e7f121c0bac76de402401dbf04
LisaMabley <lisamabley@gmail.com>
2016-05-13 12:10:03 -0500

    adds header image to inner pages

--- client/login.js
+++ client/login.js
@@ -4,19 +4,4 @@ var app = angular.module('loginApp', []);
 // Init controller
 app.controller('LoginController', ['$http', function($http) {
   var controller = this;
-  // controller.testMessage = 'LALALA';
-
-  // controller.generateSampleCoopData = function() {
-  //   console.log('ADDING SAMPLE COOP DATA');
-  //   $http.post('/coops/addSampleData');
-  // }
-  //
-  // controller.generateSampleCoopData();
-
-  // if () {
-  //   controller.loginFailureAlert = {};
-  //
-  // } else {
-  //
-  // }
 }]);
